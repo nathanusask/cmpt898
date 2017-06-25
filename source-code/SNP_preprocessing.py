f30171cfcb3f8e8817f1cdfa6264d61fd9fc221a
Nathan Yang <nathan.x.yang@outlook.com>
2017-06-24 22:25:51 -0600

    SNP preprocess updated

--- source-code/SNP_preprocessing.py
+++ source-code/SNP_preprocessing.py
@@ -9,4 +9,22 @@ Pre-processing of VCF file to extract SNP data with only ID, POS, REF and ALT st
 import vcf
 import csv
 
+with open('.vcf', 'r') as vcffile:
+	vcf_reader = vcf.Reader(vcffile)
+	with open('SNP_filter.csv', 'w') as csvfile:
+		fieldnames = ['ID', 'POS', 'REF', 'ALT']
+		csvWriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		csvWriter.writeheader()
+		for record in vcf_reader:
+			if record.is_indel:
+				continue
+			if not record.is_snp:
+				continue
+			csvWriter.writerow({'ID': record.ID,
+					'POS': record.POS,
+					'REF': record.REF,
+					'ALT': record.ALT
+					})
+	csvfile.close()
+vcffile.close()
 
