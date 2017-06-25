40fa68be692c6f1191ed463fd70651b021e73cbc
Nathan Yang <nathan.x.yang@outlook.com>
2017-06-24 22:11:27 -0600

    SNP preprocessing python code has been added

---
+++ source-code/SNP_preprocessing.py
@@ -0,0 +1,12 @@
+#author: Nathan Yang
+#Date created: Jun 24, 2017
+'''
+This python program requires PyVCF to be installed
+
+Pre-processing of VCF file to extract SNP data with only ID, POS, REF and ALT stored
+'''
+
+import vcf
+import csv
+
+
