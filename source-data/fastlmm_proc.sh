b9616685401a4fda7e06c2fd8d5f891f38559c06
Nathan Yang <nathan.x.yang@outlook.com>
2017-08-03 15:33:29 -0600

    adding fastlmm_proc.sh

---
+++ source-data/fastlmm_proc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./fastlmm_script.py 2016Nepal.bed 2016Nepal_dte.phe fl_2016Nepal_dte 'Manhattan Plot from FaST-LMM on 2016 Nepal DTE'
+
+./fastlmm_script.py 2016Nepal.bed 2016Nepal_gdd.phe fl_2016Nepal_gdd 'Manhattan Plot from FaST-LMM on 2016 Nepal GDD'
+
+./fastlmm_script.py 2016Rosthern.bed 2016Rosthern_dte.phe fl_2016Rosthern_dte 'Manhattan Plot from FaST-LMM on 2016 Rosthern DTE'
+
+./fastlmm_script.py 2016Rosthern.bed 2016Rosthern_gdd.phe fl_2016Rosthern_gdd 'Manhattan Plot from FaST-LMM on 2016 Rosthern GDD'
+
+./fastlmm_script.py 2016Sutherland.bed 2016Sutherland_dte.phe fl_2016Sutherland_dte 'Manhattan Plot from FaST-LMM on 2016 Sutherland DTE'
+
+./fastlmm_script.py 2016Sutherland.bed 2016Sutherland_gdd.phe fl_2016Sutherland_gdd 'Manhattan Plot from FaST-LMM on 2016 Sutherland GDD'
+
+./fastlmm_script.py 2017Rosthern.bed 2017Rosthern_dte.phe fl_2017Rosthern_dte 'Manhattan Plot from FaST-LMM on 2017 Rosthern DTE'
+
+./fastlmm_script.py 2017Rosthern.bed 2017Rosthern_gdd.phe fl_2017Rosthern_gdd 'Manhattan Plot from FaST-LMM on 2017 Rosthern GDD'
+
+./fastlmm_script.py 2017Sutherland.bed 2017Sutherland_dte.phe fl_2017Sutherland_dte 'Manhattan Plot from FaST-LMM on 2017 Sutherland DTE'
+
+./fastlmm_script.py 2017Sutherland.bed 2017Sutherland_gdd.phe fl_2017Sutherland_gdd 'Manhattan Plot from FaST-LMM on 2017 Sutherland GDD'
