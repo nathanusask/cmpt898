0d320f02c181012d804cb4687f127df0f0b6e920
Nathan Yang <nathan.x.yang@outlook.com>
2017-08-03 15:32:39 -0600

    adding draw_plots_plink.sh

---
+++ source-data/draw_plots_plink.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+../source-code/draw_from_plink.py 2016Nepal_DTE.qassoc plink_2016Nepal_dte 'Manhattan Plot from PLINK on 2016 Nepal DTE'
+
+../source-code/draw_from_plink.py 2016Rosthern_DTE.qassoc plink_2016Rosthern_dte 'Manhattan Plot from PLINK on 2016 Rosthern  DTE'
+
+../source-code/draw_from_plink.py 2016Sutherland_DTE.qassoc plink_2016Sutherland_dte 'Manhattan Plot from PLINK on 2016 Sutherland DTE'
+
+../source-code/draw_from_plink.py 2017Rosthern_DTE.qassoc plink_2017Rosthern_dte 'Manhattan Plot from PLINK on 2016 Rosthern  DTE'
+
+../source-code/draw_from_plink.py 2017Sutherland_DTE.qassoc plink_2017Sutherland_dte 'Manhattan Plot from PLINK on 2016 Sutherland DTE'
+
+../source-code/draw_from_plink.py 2016Nepal_GDD.qassoc plink_2016Nepal_gdd 'Manhattan Plot from PLINK on 2016 Nepal GDD'
+
+../source-code/draw_from_plink.py 2016Rosthern_GDD.qassoc plink_2016Rosthern_gdd 'Manhattan Plot from PLINK on 2016 Rosthern  GDD'
+
+../source-code/draw_from_plink.py 2016Sutherland_GDD.qassoc plink_2016Sutherland_gdd 'Manhattan Plot from PLINK on 2016 Sutherland GDD'
+
+../source-code/draw_from_plink.py 2017Rosthern_GDD.qassoc plink_2017Rosthern_gdd 'Manhattan Plot from PLINK on 2016 Rosthern  GDD'
+
+../source-code/draw_from_plink.py 2017Sutherland_GDD.qassoc plink_2017Sutherland_gdd 'Manhattan Plot from PLINK on 2016 Sutherland GDD'
+
